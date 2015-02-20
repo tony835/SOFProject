f614704c0ab6bd817871422e7d4d5267608b651f
maria <maria@147.94.233.131>
2015-02-20 15:35:32 +0100

    modification du pom.xml

--- Projet-SOF/src/main/java/domain/Person.java
+++ Projet-SOF/src/main/java/domain/Person.java
@@ -1,23 +1,19 @@
 package domain;
 
-import java.util.Date;
 
 import javax.persistence.Access;
 import javax.persistence.AccessType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.validation.constraints.Past;
+import javax.persistence.Id;
 
-import org.hibernate.validator.constraints.Email;
-import org.springframework.format.annotation.DateTimeFormat;
+
 
 
 @Entity
 @Access(AccessType.PROPERTY)
 public class Person{
+	@Id
+	private int id;
 
 	
 }
