15ddc5b7a5accb8fc2b15c88131eaaf4f2175115
maria <maria@147.94.233.94>
2015-03-03 16:17:52 +0100

    ajout des services de formation

--- Projet-SOF/src/main/java/repositories/FormationDao.java
+++ Projet-SOF/src/main/java/repositories/FormationDao.java
@@ -1,16 +1,13 @@
 package repositories;
 
-import java.util.Collection;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Formation;
-import domain.Person;
 
 @Repository
-public interface FormationDao extends JpaRepository<Formation, Integer> {
+public interface FormationDao extends JpaRepository<Formation, String> {
 
 	
 }
