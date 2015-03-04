aef9d7fb523deef384d39b37e58c90d01ddc95fa
alexandre <alexandre@147.94.235.71>
2015-03-04 13:51:57 +0100

    COrrection d'une erreur.

--- Projet-SOF/src/main/java/repositories/ObjectDao.java
+++ Projet-SOF/src/main/java/repositories/ObjectDao.java
@@ -1,15 +1,10 @@
 package repositories;
 
-import java.util.Collection;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import domain.Person;
 
 @Repository
-public interface ObjectDao extends JpaRepository<Object, Integer> {
-
+public interface ObjectDao extends JpaRepository<domain.Object, Integer> {
 	
 }
