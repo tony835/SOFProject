ddc6e96f44e6175173be0d1664fde730390118f6
Quentin Cheynet <cheynet.q@gmail.com>
2015-03-06 17:20:35 +0100

    fusion arbreFormationController

--- Projet-SOF/src/main/java/controllers/ArbreFormationController.java
+++ Projet-SOF/src/main/java/controllers/ArbreFormationController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -7,8 +8,11 @@ import javax.validation.Valid;
 
 import org.javatuples.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomCollectionEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,10 +23,13 @@ import services.FilsService;
 import services.FormationService;
 import services.ObjectService;
 import services.PersonService;
+import services.TypeObjectService;
+import domain.FieldObject;
 import domain.Fils;
 import domain.Formation;
-import domain.Object;
 import domain.Person;
+import domain.TypeObject;
+import domain.User;
 
 @Controller
 @RequestMapping("/arbreFormation")
@@ -45,6 +52,12 @@ public class ArbreFormationController extends AbstractController {
 
 	@Autowired
 	FilsService pereFilsService;
+	
+	@Autowired
+	TypeObjectService typeService;
+
+	@Autowired
+	User user;
 
 	/**
 	 * Liste touts les formations.
@@ -55,7 +68,7 @@ public class ArbreFormationController extends AbstractController {
 	public ModelAndView allFormation(@RequestParam String code) {
 		ModelAndView result;
 
-		Collection<Object> objects = objectService.objectsNonLiee(code);
+		Collection<domain.Object> objects = objectService.objectsNonLiee(code);
 		result = new ModelAndView("arbreFormation/list");
 		result.addObject("ObjetNonLie", objects);
 		List<Pair<domain.Object, Integer>> arbreFormations = formationService.getListFormationIndente(code);
@@ -160,5 +173,91 @@ public class ArbreFormationController extends AbstractController {
 		}
 		ModelAndView result = new ModelAndView("redirect:gestionFils.htm?code="+codeEnCours);
 		return result;
+	}
+	
+	@RequestMapping(value = "/create.htm", method = RequestMethod.GET)
+	public String create() {
+		return "tmpObjectCreation/createObject";
+	}
+
+	@RequestMapping(value = "/create.htm", method = RequestMethod.POST)
+	public String saveNewObject(@ModelAttribute @Valid domain.Object o, BindingResult result) {
+		if(result.hasErrors()) {
+			return "formation/list.htm";
+		}
+
+		objectService.save(o, user);
+
+		return "formation/list";
+
+	}
+	
+	
+	@RequestMapping(value = "/editsons.htm", method = RequestMethod.GET)
+	public String manageSons() {
+		return "tmpObjectCreation/editSons";
+	}
+
+	@RequestMapping(value = "/editsons.htm", method = RequestMethod.POST)
+	public String saveObjectSons(@ModelAttribute @Valid domain.Object o, BindingResult result) {
+		if(result.hasErrors()) {
+			return "formation/list.htm";
+		}
+		//objectService.save(o, user);
+		return "formation/list";
+	}
+
+	@ModelAttribute("myobject")
+	public domain.Object newObject(
+			@RequestParam(value = "code", required = false) String code) {
+				
+		if (code != null) {
+			return objectService.findOne(code);
+		}
+		domain.Object o = new domain.Object();
+		o.setCode("");
+		o.setName("");
+		o.setVersion("");
+		o.setMutualisable(false);
+		o.setContexte(null);
+		o.setTypeObject(new TypeObject());
+		o.setAllFils(new ArrayList<Fils>());
+		o.setFieldObjects(new ArrayList<FieldObject>());
+
+		return o;
+	}
+
+	@ModelAttribute("typesList")
+	public List<TypeObject> productTypes() {
+		return (List<TypeObject>) typeService.findAll();
+	}
+	
+	@ModelAttribute("NonLinkedObjectList")
+	public List<domain.Object> nlObjectList() {
+		return (List<domain.Object>) objectService.objectsNonLiee(null);
+	}
+	
+	@ModelAttribute("mutualisableObjectList")
+	public List<domain.Object> mObjectList() {
+		return (List<domain.Object>) objectService.findMutualisableObjects(null);
+	}
+
+	@ModelAttribute("user")
+	public User newUser() {
+		return user;
+	}
+
+	@InitBinder
+	protected void initBinder(WebDataBinder binder) {
+		binder.registerCustomEditor(List.class, "typeObject", new CustomCollectionEditor(List.class)
+		{
+			@Override
+			protected Object convertElement(Object element)
+			{
+				List<TypeObject> retour = new ArrayList<TypeObject>();
+				retour.add(typeService.findOne((String) element));
+				return retour;
+			}
+		});
 	}
 }
