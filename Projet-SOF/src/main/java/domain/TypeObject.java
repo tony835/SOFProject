e7029068dba6944c450c2f5767a33ee83234396a
Salah13 <salah.hassnaoui@yahoo.com>
2015-03-10 18:01:00 +0100

    mise a jour

--- Projet-SOF/src/main/java/domain/TypeObject.java
+++ Projet-SOF/src/main/java/domain/TypeObject.java
@@ -31,7 +31,7 @@ public class TypeObject {
 	@Column(name="ContentModel")
 	String modelContenu;
 	
-	@Column(name="Erreur_descritpion")
+	@Column(name="Erreur_description")
 	String descError;
 	
 	@OneToMany(mappedBy="typeObject")
