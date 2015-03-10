a4559f02ca92a99163ba9f567a172d674bf756e7
tony <neotony83@gmail.com>
2015-03-10 16:18:37 +0100

    Erreur de guillaume résolue!

--- Projet-SOF/src/main/java/controllers/App2FormationController.java
+++ Projet-SOF/src/main/java/controllers/App2FormationController.java
@@ -1,8 +1,15 @@
 package controllers;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -15,7 +22,6 @@ import services.PersonService;
 import domain.Formation;
 import domain.Object;
 import domain.User;
-
 @Controller
 @RequestMapping("/visualisation")
 public class App2FormationController {
@@ -55,13 +61,48 @@ public class App2FormationController {
 		
 		formations.clear();
 		formations = null;
-
+		getFormation_Field();
 		result = new ModelAndView("formation/offre");
 		result.addObject("FormationVisible", objetsVisible);
 
 		return result;
 	}
 	
+ 	private Collection <String> getFormation_Field(){
+ 		SAXBuilder sxb = new SAXBuilder();
+ 		Document document = null;
+ 		try { 
+ 		document = sxb.build(getClass().getResource("/configApp.xml"));
+ 		} catch (JDOMException | IOException e) {
+ 			e.printStackTrace();
+ 		}
+ 		
+ 		Element racine = document.getRootElement();
+ 		
+ 		List <String> f = getAllDesigner(racine);
+ 		
+ 		for(String e : f){
+ 			System.out.println(e);
+ 		}
+ 		
+ 		return null;
+ 		
+ 	}
+ 	
+ 	private List<String> getAllDesigner(Element racine) {
+ 		List<Element> listDiploma = racine.getChild("diploma_type").getChildren(
+ 				"name");
+ 
+ 		Iterator<Element> i = listDiploma.iterator();
+ 		List<String> ret = new ArrayList<String>();
+ 
+ 		while (i.hasNext()) {
+ 			Element courant = (Element) i.next();
+ 			ret.add(courant.getValue());
+ 		}
+ 		return ret;
+ 	}
+
 	@ModelAttribute("user")
 	public User newUser() {
 		return user;
