a09a9e60fd1bb376f33669812ec13fac4097f86b
alexandre <alexandre@192.168.0.2>
2015-03-24 10:35:28 +0100

    Ajout de la liste des filed sélectionné par le code

--- Projet-SOF/src/main/java/services/FieldService.java
+++ Projet-SOF/src/main/java/services/FieldService.java
@@ -2,14 +2,22 @@ package services;
 
 
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import repositories.FieldDao;
 import domain.Field;
+import domain.TypeObject;
 
 
 @Service
@@ -35,6 +43,37 @@ public class FieldService {
 		
 		fildDao.delete(f2) ;
 	}
+	private List<String> listFieldByCode(Element racine, String code) {
+		List<String> ids = new ArrayList<String>();
+		List<Element> listTypesObject = racine.getChildren("object_type");
+		Iterator<Element> i = listTypesObject.iterator();
+		while (i.hasNext()) {
+			Element courant = (Element) i.next();
+			TypeObject typeObject = new TypeObject();
+			if(courant.getChild("code").getText().equals(code)){
+				List<Element> listField = courant.getChildren("field");
+				Iterator<Element> j = listField.iterator();
+				typeObject.setFields(new ArrayList<Field>());
+				while (j.hasNext()) {
+					Element courant2 = (Element) j.next();
+					ids.add(courant2.getChild("Id").getText());
+				}
+			}
+		}
+		return ids;
+	}
+	
+	public List<String> getListFieldByCode(String code){
+		SAXBuilder sxb = new SAXBuilder();
+		Document document = null;
+		try {
+			document = sxb.build(getClass().getResource("/configApp.xml"));
+		} catch (JDOMException | IOException e) {
+			e.printStackTrace();
+		}
 
+		Element racine = document.getRootElement();
+		return listFieldByCode(racine, code);
+	}
 
 }
